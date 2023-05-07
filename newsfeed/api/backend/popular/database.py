b4655768bf71832e3e3ffb258acfe2b8f6af78fe
Mickael GASPAR <migatsar@gmail.com>
2023-05-08 00:11:39 +0200

    Start backend by copying popular use-case

---
+++ newsfeed/api/backend/popular/database.py
@@ -0,0 +1,16 @@
+""" Instanciate the connection to the DB."""
+
+import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# MongoDB setup: Async connection with Motor client
+# Connection to the cluster
+mongo_host = os.getenv("MONGO_CLIENT_HOST")  # Cloud Atlas connection
+# mongo_host = os.getenv("MONGO_LOCAL_HOST")  # Local MongoDB connection
+mongodb_client = AsyncIOMotorClient(mongo_host)
+
+# Connection to the DB
+mongodb = mongodb_client["NYTimes"]
+# db_name = os.getenv("MONGO_DB_NAME")
+# mongodb = mongodb_client[db_name]
